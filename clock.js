c6fcc4f71972d411fe8808e63d2e53efb7982dfd
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2023-11-06 09:29:08 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -257,13 +257,10 @@ function displayCurrentTime() {
   const a = timeString.split(':');
   const currentTime = `${a[0]}.${a[1]}`;
   const AM = a[2].includes('AM');
-
   // console.log(timeString)
   // process.stdout.clearLine()
-  // process.stdout.write
-//("\n"+timeString")
-
-
+  // process.stdout.write("\n"+timeString)
+  
 
  
   const matchingNames = data.filter((item) => item.time === currentTime);

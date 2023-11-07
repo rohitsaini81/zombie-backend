3e5e7662981b324a296f2aa5ec81dc8988909b40
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2023-11-07 17:42:17 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,47 +1,47 @@
-console.log(1)
+import axios from 'axios';
+
 let nowTime = new Date();
-//  clock function with 12 hour format
+
+// Clock function with 12-hour format
 const clock = (hour, minutes, seconds, AMPM) => {
-    const date = new Date();
+  const date = new Date();
+  date.setHours(hour + (AMPM === 'PM' && hour !== 12 ? 12 : 0));
+  date.setMinutes(minutes);
+  date.setSeconds(seconds);
+
+  setInterval(() => {
+    seconds++;
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes === 60) {
+      hour++;
+      minutes = 0;
+    }
+    if (hour === 12 && minutes === 0 && seconds === 0) {
+      AMPM = AMPM === 'AM' ? 'PM' : 'AM';
+    }
+    if (hour === 13) {
+      hour = 1;
+    }
     date.setHours(hour + (AMPM === 'PM' && hour !== 12 ? 12 : 0));
     date.setMinutes(minutes);
     date.setSeconds(seconds);
-  
-    setInterval(() => {
-      seconds++;
-      if (seconds === 60) {
-        minutes++;
-        seconds = 0;
-      }
-      if (minutes === 60) {
-        hour++;
-        minutes = 0;
-      }
-      if (hour === 12 && minutes === 0 && seconds === 0) {
-        AMPM = AMPM === 'AM' ? 'PM' : 'AM';
-      }
-      if (hour === 13) {
-        hour = 1;
-      }
-      date.setHours(hour + (AMPM === 'PM' && hour !== 12 ? 12 : 0));
-      date.setMinutes(minutes);
-      date.setSeconds(seconds);
-      nowTime = date.toLocaleTimeString();
-      // console.log(nowTime);
+    nowTime = date.toLocaleTimeString();
+  }, 1000);
+};
 
-    }, 1000);
-  };
-  
+const timeapi = () => {
+  axios.get('https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata')
+    .then((response) => {
+      const { hour, minute, seconds } = response.data;
+      clock(hour, minute, seconds, "AM");
+      console.log("Time updated");
+    })
+    .catch((error) => {
+      console.error("Failed to fetch time:", error);
+    });
+};
 
-  const timeapi = () => {
-    const uri = 'https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata';
-    fetch(uri)
-      .then((res) => res.json())
-      .then((data) => {
-        const { hour, minute, seconds } = data;
-        clock(hour, minute, seconds, "AM");
-        console.log("time updated")
-      })
-    }
-  
-export { timeapi ,nowTime };
+export { timeapi, nowTime };
